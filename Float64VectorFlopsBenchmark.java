5ed098613980d29fcc8feff8f8d4133d2032a85c
RINEARN <rinearn@gmail.com>
2018-09-29 23:21:31 +0900

    ベクトル演算性能計測用のベンチマークコードを追加

---
+++ Float64VectorFlopsBenchmark.java
@@ -0,0 +1,161 @@
+/*
+ * A benchmarking program for measuring the peak performance of 
+ * 64-bit floating-point vector operations of Vnano Engine.
+ * Vnano処理系の倍精度浮動小数点ベクトル演算ピーク性能計測用ベンチマークプログラム
+ * --------------------------------------------------------------------------------
+ * This file is released under CC0.
+ * Written in 2018 by RINEARN (Fumihiro Matsui)
+ * --------------------------------------------------------------------------------
+ * 
+ * Preparing - 事前準備
+ * 
+ *     Execute "build.bat" or "build.sh" in advance to generate "Vnano.jar".
+ *     あらかじめ build.bat か build.sh を実行し、Vnano.jar を生成しておいて下さい。
+ * 
+ * 
+ * How to compile this code - このコードのコンパイル方法 :
+ * 
+ *     javac Float64VectorFlopsBenchmark.java -encoding UTF-8
+ * 
+ * 
+ * How to execute this code - このコードの実行方法 :
+ * 
+ *     java -classpath ".;Vnano.jar" Float64VectorFlopsBenchmark
+ * 
+ *         or, (depending on your environment)
+ * 
+ *     java -classpath ".:Vnano.jar" Float64VectorFlopsBenchmark
+ * 
+ * 
+ * Expected result - 正常な実行結果 :
+ * 
+ *     OPERATING_SPEED = 4.8389764430687805 [GFLOPS]
+ *     REQUIRED_TIME = 42.323 [SEC]
+ *     TOTAL_OPERATIONS = 204800000000 [xFLOAT64_ADD]
+ *     VECTOR_SIZE = 2048 [x64BIT]
+ *     OPERATED_VALUES = { 1.0E8, 2.0E8, 3.0E8, ... , 2.047E11, 2.048E11 }
+ * 
+ *     * Values of OPERATION_SPEED and REQUIRED_TIME are dependent on your environment.
+ *     ※OPERATION_SPEED と REQUIRED_TIME の値は環境に依存します。
+ * 
+ */
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Float64VectorFlopsBenchmark {
+	
+	// Methods accessed from the script as an external functions.
+	// スクリプト側から外部関数としてアクセスするメソッドとフィールド
+	public static void output(String paramName, long score, String unit) {
+		System.out.println(paramName + " = " + score + " [" + unit + "]");
+	}
+	public static void output(String paramName, double score, String unit) {
+		System.out.println(paramName + " = " + score + " [" + unit + "]");
+	}
+	public static void output(String paramName, double[] values) {
+		int n = values.length;
+		System.out.print(paramName + " = { ");
+		System.out.print(values[0] + ", " + values[1] + ", " + values[2] + ", ");
+		System.out.print("... , " + values[n-2] + ", " + values[n-1]);
+		System.out.println(" }");
+	}
+	public static long time() {
+		return System.nanoTime() / 1000000l;
+	}
+
+	public static void main(String[] args) {
+
+		// Get ScriptEngine of Vnano from ScriptEngineManager.
+		// ScriptEngineManagerでVnanoのスクリプトエンジンを検索して取得
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("vnano");
+		if (engine == null) {
+			System.err.println("Script engine not found.");
+			return;
+		}
+
+		// Connect methods to the script engine as external functions.
+		// メソッドを外部関数としてスクリプトエンジンに接続
+		try {
+			engine.put(
+				"output(string,int,string)", 
+				Float64VectorFlopsBenchmark.class.getMethod("output", String.class, long.class, String.class)
+			);
+			engine.put(
+				"output(string,double,string)", 
+				Float64VectorFlopsBenchmark.class.getMethod("output", String.class, double.class, String.class)
+			);
+			engine.put(
+				"output(string,double[])", 
+				Float64VectorFlopsBenchmark.class.getMethod("output", String.class, double[].class)
+			);
+			engine.put(
+				"time()", 
+				Float64VectorFlopsBenchmark.class.getMethod("time")
+			);
+
+		} catch (NoSuchMethodException e){
+			System.err.println("Method not found.");
+			e.printStackTrace();
+			return;
+		}
+
+
+		// Create a script code.
+		// スクリプトコードを用意
+		String scriptCode =
+
+			"int VECTOR_SIZE = 2048;                                                 " + 
+			"int LOOP_N = 1000*1000;                                                 " + 
+			"int FLOP_PER_LOOP = VECTOR_SIZE * 100;                                  " + 
+			"int TOTAL_FLOP = FLOP_PER_LOOP * LOOP_N;                                " + 
+			"                                                                        " + 
+			"double x[VECTOR_SIZE];                                                  " + 
+			"double y[VECTOR_SIZE];                                                  " + 
+			"for (int i=0; i<VECTOR_SIZE; i++) {                                     " + 
+			"  x[i] = 0.0;                                                           " + 
+			"  y[i] = i + 1.0;                                                       " + 
+			"}                                                                       " + 
+			"                                                                        " + 
+			"int beginTime = time();                                                 " + 
+			"                                                                        " + 
+			"for (int i=0; i<LOOP_N; ++i) {                                          " + 
+			"                                                                        " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"  x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y; x+=y;           " + 
+			"                                                                        " + 
+			"}                                                                       " + 
+			"                                                                        " + 
+			"int endTime = time();                                                   " + 
+			"double requiredTime = (endTime - beginTime) / 1000.0;                   " + 
+			"double flops = TOTAL_FLOP / requiredTime;                               " + 
+			"                                                                        " + 
+			"output(\"OPERATING_SPEED\", flops/(1000.0*1000.0*1000.0), \"GFLOPS\");  " + 
+			"output(\"REQUIRED_TIME\", requiredTime, \"SEC\");                       " + 
+			"output(\"TOTAL_OPERATIONS\", TOTAL_FLOP, \"xFLOAT64_ADD\");             " + 
+			"output(\"VECTOR_SIZE\", VECTOR_SIZE, \"x64BIT\");                       " + 
+			"output(\"OPERATED_VALUES\", x);                                         " ;
+
+
+		// Run the script code by the script engine of Vnano.
+		// Vnanoのスクリプトエンジンにスクリプトコードを渡して実行
+		try{
+			engine.eval(scriptCode);
+			
+		} catch (ScriptException e) {
+			System.err.println("Scripting error occurred.");
+			e.printStackTrace();
+			return;
+		}
+	}
+}
