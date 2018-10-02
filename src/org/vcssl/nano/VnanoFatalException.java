0a2ac9b5de81cd3fd5fa4b192b46f6b9a5641b12
RINEARN <rinearn@gmail.com>
2018-10-03 04:07:06 +0900

    処理系の異常に起因する例外クラスを追加

---
+++ src/org/vcssl/nano/VnanoFatalException.java
@@ -0,0 +1,21 @@
+package org.vcssl.nano;
+
+/**
+ * 実装上の異常に起因する例外です。
+ * この例外が発生した場合は、処理系が開発時に想定されていない状態や挙動に陥った事を意味しているため、
+ * 処理系の実装を修正する必要があります。
+ * 処理系が実装が正常であれば、実行するスクリプトの内容によらず、この例外は本来発生しないべきです。
+ */
+@SuppressWarnings("serial")
+public class VnanoFatalException extends RuntimeException {
+
+	public VnanoFatalException() {
+		super();
+	}
+	public VnanoFatalException(String message) {
+		super(message);
+	}
+	public VnanoFatalException(Throwable cause) {
+		super(cause);
+	}
+}
