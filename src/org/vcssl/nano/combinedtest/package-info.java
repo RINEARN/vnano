a5e45d6fe59647ca6cf53376506e2a39b816c77e
RINEARN <rinearn@gmail.com>
2019-09-23 18:19:02 +0900

    Add: new package "combinedtest" for combined tests of the script engine

---
+++ src/org/vcssl/nano/combinedtest/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <span class="lang-en">
+ * The package of combined tests of the script engine of the Vnano.
+ * </span>
+ * <span class="lang-ja">
+ * Vnanoのスクリプトエンジンの結合テストがまとめられたパッケージです
+ * </span>
+ * .
+ */
+package org.vcssl.nano.combinedtest;
