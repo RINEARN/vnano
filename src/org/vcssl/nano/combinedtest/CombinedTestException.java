220e9df3034f94aee7baaa0ea2312860770da1e2
RINEARN <rinearn@gmail.com>
2020-04-11 21:53:42 +0900

    Add: checking methods and error handlings for tests of vector operations

--- src/org/vcssl/nano/combinedtest/CombinedTestException.java
+++ src/org/vcssl/nano/combinedtest/CombinedTestException.java
@@ -1,5 +1,7 @@
 package org.vcssl.nano.combinedtest;
 
+import java.util.Arrays;
+
 import org.vcssl.nano.VnanoException;
 
 @SuppressWarnings("serial")
@@ -9,7 +11,7 @@ public class CombinedTestException extends RuntimeException {
 		super(e);
 	}
 
-	CombinedTestException(Object resultValue, Object correctValue, String testName, String scriptCode) {
+	CombinedTestException(String resultValue, String correctValue, String testName, String scriptCode) {
 		super(
 				"The result value \""+ resultValue + "\" should be \"" + correctValue + "\"."
 		);
@@ -22,19 +24,40 @@ public class CombinedTestException extends RuntimeException {
 		System.err.println(scriptCode);
 		System.err.println("");
 	}
+	CombinedTestException(String[] resultValue, String[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
+	}
 	CombinedTestException(int resultValue, int correctValue, String testName, String scriptCode) {
 		this(Integer.toString(resultValue), Integer.toString(correctValue), testName, scriptCode);
+	}
+	CombinedTestException(int[] resultValue, int[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
 	}
 	CombinedTestException(long resultValue, long correctValue, String testName, String scriptCode) {
 		this(Long.toString(resultValue), Long.toString(correctValue), testName, scriptCode);
 	}
+	CombinedTestException(long[] resultValue, long[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
+	}
 	CombinedTestException(float resultValue, float correctValue, String testName, String scriptCode) {
 		this(Float.toString(resultValue), Float.toString(correctValue), testName, scriptCode);
+	}
+	CombinedTestException(float[] resultValue, float[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
 	}
 	CombinedTestException(double resultValue, double correctValue, String testName, String scriptCode) {
 		this(Double.toString(resultValue), Double.toString(correctValue), testName, scriptCode);
 	}
+	CombinedTestException(double[] resultValue, double[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
+	}
 	CombinedTestException(boolean resultValue, boolean correctValue, String testName, String scriptCode) {
 		this(Boolean.toString(resultValue), Boolean.toString(correctValue), testName, scriptCode);
+	}
+	CombinedTestException(boolean[] resultValue, boolean[] correctValue, String testName, String scriptCode) {
+		this(Arrays.toString(resultValue), Arrays.toString(correctValue), testName, scriptCode);
+	}
+	CombinedTestException(Object resultValue, Object correctValue, String testName, String scriptCode) {
+		this(resultValue.toString(), correctValue.toString(), testName, scriptCode);
 	}
 }
