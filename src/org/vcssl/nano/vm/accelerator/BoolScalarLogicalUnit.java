c73afc53228c1231968a36e4b526f7a57ecbb994
RINEARN <rinearn@gmail.com>
2020-04-14 12:06:30 +0900

    Refactor: VM/Accelerator: short-circuit evaluations for AND/OR instructions

--- src/org/vcssl/nano/vm/accelerator/BoolScalarLogicalUnit.java
+++ src/org/vcssl/nano/vm/accelerator/BoolScalarLogicalUnit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2017-2018 RINEARN (Fumihiro Matsui)
+ * Copyright(C) 2017-2020 RINEARN (Fumihiro Matsui)
  * This software is released under the MIT License.
  */
 
@@ -86,19 +86,17 @@ public class BoolScalarLogicalUnit extends AcceleratorExecutionUnit {
 			super(container0, container1, container2, synchronizer, nextNode);
 		}
 		public final AcceleratorExecutionNode execute() {
-			this.synchronizer.synchronizeFromCacheToMemory();
 
 			// 注： 短絡評価により、左辺(container1) が false の場合には
 			//      右辺(container2) は null になっている可能性があるので、
-			//      左辺から結果が自明な場合には右辺を参照してはいけない
+			//      左辺から結果が自明な場合には右辺を参照してはいけない。
 			//      (アクセスコストの面からも恐らく不利)
-			boolean leftHandValue = this.container1.getData()[ this.container1.getOffset() ];
-			if (leftHandValue) {
-				this.container0.getData()[ this.container0.getOffset() ] =
-					leftHandValue & this.container2.getData()[ this.container2.getOffset() ] ;
-			} else {
-				this.container0.getData()[ this.container0.getOffset() ] = false;
-			}
+			//      そのため、以下の && 演算子を & 演算子にしてはいけない。
+
+			this.synchronizer.synchronizeFromCacheToMemory();
+			this.container0.getData()[ this.container0.getOffset() ] =
+			this.container1.getData()[ this.container1.getOffset() ] &&
+			this.container2.getData()[ this.container2.getOffset() ] ;
 			this.synchronizer.synchronizeFromMemoryToCache();
 			return this.nextNode;
 		}
@@ -114,19 +112,17 @@ public class BoolScalarLogicalUnit extends AcceleratorExecutionUnit {
 		}
 
 		public final AcceleratorExecutionNode execute() {
-			this.synchronizer.synchronizeFromCacheToMemory();
 
-			// 注： 短絡評価により、左辺(container1) が true の場合には
+			// 注： 短絡評価により、左辺(container1) が false の場合には
 			//      右辺(container2) は null になっている可能性があるので、
-			//      左辺から結果が自明な場合には右辺を参照してはいけない
+			//      左辺から結果が自明な場合には右辺を参照してはいけない。
 			//      (アクセスコストの面からも恐らく不利)
-			boolean leftHandValue = this.container1.getData()[ this.container1.getOffset() ];
-			if (leftHandValue) {
-				this.container0.getData()[ this.container0.getOffset() ] = true;
-			} else {
-				this.container0.getData()[ this.container0.getOffset() ] =
-					leftHandValue | this.container2.getData()[ this.container2.getOffset() ] ;
-			}
+			//      そのため、以下の || 演算子を | 演算子にしてはいけない。
+
+			this.synchronizer.synchronizeFromCacheToMemory();
+			this.container0.getData()[ this.container0.getOffset() ] =
+			this.container1.getData()[ this.container1.getOffset() ] ||
+			this.container2.getData()[ this.container2.getOffset() ] ;
 			this.synchronizer.synchronizeFromMemoryToCache();
 			return this.nextNode;
 		}
