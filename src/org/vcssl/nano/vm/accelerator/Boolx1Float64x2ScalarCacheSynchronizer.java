d5cbe0015f05af1b38a91b046129126a5db2243c
RINEARN <rinearn@gmail.com>
2020-04-14 09:58:41 +0900

    Fix: VM/Accelerator: incorrect type of cache-synchronizer
    The data-type of caches passed to Float64ScalarComparisonUnit
    is Float64, but the cache-synchronizer created in the unit
    had been Int64 type (should be Float64 type).

---
+++ src/org/vcssl/nano/vm/accelerator/Boolx1Float64x2ScalarCacheSynchronizer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright(C) 2017-2018 RINEARN (Fumihiro Matsui)
+ * This software is released under the MIT License.
+ */
+
+package org.vcssl.nano.vm.accelerator;
+
+import org.vcssl.nano.vm.memory.DataContainer;
+
+public class Boolx1Float64x2ScalarCacheSynchronizer extends CacheSynchronizer {
+	private final DataContainer<boolean[]> container0;
+	private final DataContainer<double[]> container1;
+	private final DataContainer<double[]> container2;
+	private final BoolScalarCache cache0;
+	private final Float64ScalarCache cache1;
+	private final Float64ScalarCache cache2;
+	private final boolean cacheSyncEnabled0;
+	private final boolean cacheSyncEnabled1;
+	private final boolean cacheSyncEnabled2;
+
+	@SuppressWarnings("unchecked" )
+	public Boolx1Float64x2ScalarCacheSynchronizer(DataContainer<?>[] containers, Object[] caches,
+			boolean[] cacheSyncEnabled) {
+
+		container0 = (DataContainer<boolean[]>)containers[0];
+		container1 = (DataContainer<double[]>)containers[1];
+		container2 = (DataContainer<double[]>)containers[2];
+		cache0 = (BoolScalarCache)caches[0];
+		cache1 = (Float64ScalarCache)caches[1];
+		cache2 = (Float64ScalarCache)caches[2];
+		cacheSyncEnabled0 = cacheSyncEnabled[0];
+		cacheSyncEnabled1 = cacheSyncEnabled[1];
+		cacheSyncEnabled2 = cacheSyncEnabled[2];
+	}
+
+	public final void synchronizeFromCacheToMemory() {
+		if (cacheSyncEnabled0) container0.getData()[ container0.getOffset() ] = cache0.value;
+		if (cacheSyncEnabled1) container1.getData()[ container1.getOffset() ] = cache1.value;
+		if (cacheSyncEnabled2) container2.getData()[ container2.getOffset() ] = cache2.value;
+	}
+
+	public final void synchronizeFromMemoryToCache() {
+		if (cacheSyncEnabled0) cache0.value = container0.getData()[ container0.getOffset() ];
+		if (cacheSyncEnabled1) cache1.value = container1.getData()[ container1.getOffset() ];
+		if (cacheSyncEnabled2) cache2.value = container2.getData()[ container2.getOffset() ];
+	}
+}
