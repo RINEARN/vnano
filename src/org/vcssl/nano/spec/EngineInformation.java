0484f5a83877a9d2df37718061538d2502b6315d
RINEARN <rinearn@gmail.com>
2020-06-17 23:31:13 +0900

    Update: version: 0.2.8 (beta 8)

--- src/org/vcssl/nano/spec/EngineInformation.java
+++ src/org/vcssl/nano/spec/EngineInformation.java
@@ -43,7 +43,7 @@ public class EngineInformation {
 
 	public static final String ENGINE_NAME = "RINEARN Vnano Engine";
 
-	public static final String ENGINE_VERSION = "0.2.7";
+	public static final String ENGINE_VERSION = "0.2.8";
 
 	public static final String[] EXTENTIONS = { "vnano" };
 
