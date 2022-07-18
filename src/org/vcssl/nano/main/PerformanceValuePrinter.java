03c3d75056d0023f922fbf26404e59f2df1b2993
RINEARN <commit@rinearn.com>
2022-07-19 00:44:43 +0900

    Refactor: main: clean-up/translate comments in PerformanceValuePrinter

--- src/org/vcssl/nano/main/PerformanceValuePrinter.java
+++ src/org/vcssl/nano/main/PerformanceValuePrinter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2020 RINEARN (Fumihiro Matsui)
+ * Copyright(C) 2020-2022 RINEARN
  * This software is released under the MIT License.
  */
 
@@ -21,65 +21,122 @@ import org.vcssl.nano.spec.OperationCode;
 import org.vcssl.nano.spec.PerformanceKey;
 import org.vcssl.nano.vm.VirtualMachine;
 
+
+/**
+ * The class for printing measured performance values in realtime.
+ */
 public class PerformanceValuePrinter implements Runnable {
 
-	// 累積命令カウンタの精度は int で、表現範囲が±2G回くらいなので、数秒ごとに1周する。
-	// 従って、余裕をもって毎秒数十回～百回くらいの頻度で取得して、平均をとりつつ1秒あたりのスコアを求める。
-	private static final int PROC_CNT_GETS_PER_SEC = 100; // 毎秒取得回数（1000以下で指定 / ウェイトの精度からして大まかな目安値程度）
-	private static final int PROC_CNT_INTERVAL_WAIT = 1000 / PROC_CNT_GETS_PER_SEC; // 取得ループ待ち時間（ミリ秒）
+	// Note: The precision of the counter of executed instructions is "int", so its max value is about 2 Giga counts.
+	// The max processing speed of this script engine is near the 1 Giga instructions per sec.
+	// Hence we should measure performance values for 10 ~ 100 times per sec, and we should reset the counter for each measurement.
+	// Printed results by this class are 1-sec averages of the above frequently measured values.
+
+	/** The rough value of the measurement count per 1-sec. The value must be smaller than 1000. */
+	private static final int PROC_CNT_GETS_PER_SEC = 100;
+
+	/** The rough value of the measurement interval [millisec]. */
+	private static final int PROC_CNT_INTERVAL_WAIT = 1000 / PROC_CNT_GETS_PER_SEC;
+
+	/** The sum of measured IPS (Instruction per sec) score. This sum value will be used for computing 1-sec avelage. */
 	private double procIpsSum = 0.0;
 
-
-	// サンプリングしたオペコードの総数
+	/** The total count of sampled operation codes for the profiling. */
 	private long operationCodeCountTotal;
 
-	// サンプリングしたオペコードの名前をキーとして、
-	// 重み付き検出回数（複数命令が同時に検出された時に 1/N 回とカウント）を格納するマップ
-	private HashMap<String, Double> operationCodeCountMap; // clone するので抽象的な Map ではなく HashMap
+	/**
+	 * The Map mapping each operation code to detected count of it.
+	 * Note that, when multiple operation codes are detected at the same moment, 
+	 * their count will be incremented 1/N, where N is the number of operation codes detected at the same moment.
+	 */
+	private HashMap<String, Double> operationCodeCountMap;
 
-	VnanoEngine vnanoEngine = null;
-	VirtualMachine virtualMachine = null;
-
+	/** The flag representing whether the VM speed should be printed. */
 	private boolean printsVmSpeed = false;
+
+	/** The flag representing whether the RAM usage should be printed. */
 	private boolean printsRamUsage = false;
+
+	/** The flag representing whether the instruction executed frequency should be printed. */
 	private boolean printsInstructionFrequency = false;
 
+	/** The flag representing whether we should continue to measure performance. Set false when quit to measure/print. */
 	private volatile boolean continuable = true;
 
-	private PerformanceValuePrinter(boolean printsVmSpeed, boolean printsRamUsage, boolean printsInstructionFrequency) {
+	/** Stores the reference to the script engine. */
+	private VnanoEngine vnanoEngine = null;
+
+	/** Stores the reference to the virtual machine. */
+	private VirtualMachine virtualMachine = null;
+
+
+	/**
+	 * Create an instance for printing performances of the VnanoEngine.
+	 * 
+	 * @param vnanoEngine The VnanoEngine of which performances will be measured/printed.
+	 * @param printsVmSpeed Specify true to measure/print the VM drive speed.
+	 * @param printsRamUsage Specify true to measure/print the RAM usage.
+	 * @param printsInstructionFrequency Specify true to measure/print the executed frequencies of operation codes of instructions.
+	 */
+	public PerformanceValuePrinter(VnanoEngine vnanoEngine,
+			boolean printsVmSpeed, boolean printsRamUsage, boolean printsInstructionFrequency) {
+
 		this.printsVmSpeed = printsVmSpeed;
 		this.printsRamUsage = printsRamUsage;
 		this.printsInstructionFrequency = printsInstructionFrequency;
 		this.operationCodeCountMap = new HashMap<String, Double>();
 		this.operationCodeCountTotal = 0L;
-	}
 
-	public PerformanceValuePrinter(VnanoEngine vnanoEngine,
-			boolean printsVmSpeed, boolean printsRamUsage, boolean printsInstructionFrequency) {
-
-		this(printsVmSpeed, printsRamUsage, printsInstructionFrequency);
 		this.vnanoEngine = vnanoEngine;
 	}
 
+
+	/**
+	 * Create an instance for printing performances of the VirtualMachine.
+	 * 
+	 * @param vnanoEngine The VnanoEngine of which performances will be measured/printed.
+	 * @param printsVmSpeed Specify true to measure/print the VM drive speed.
+	 * @param printsRamUsage Specify true to measure/print the RAM usage.
+	 * @param printsInstructionFrequency Specify true to measure/print the executed frequencies of operation codes of instructions.
+	 */
 	public PerformanceValuePrinter(VirtualMachine virtualMachine,
 			boolean printsVmSpeed, boolean printsRamUsage, boolean printsInstructionFrequency) {
 
-		this(printsVmSpeed, printsRamUsage, printsInstructionFrequency);
+		this.printsVmSpeed = printsVmSpeed;
+		this.printsRamUsage = printsRamUsage;
+		this.printsInstructionFrequency = printsInstructionFrequency;
+		this.operationCodeCountMap = new HashMap<String, Double>();
+		this.operationCodeCountTotal = 0L;
+
 		this.virtualMachine = virtualMachine;
 	}
 
+
+	/**
+	 * Terminates measuring/printing performances.
+	 */
 	public void terminate() {
 		this.continuable = false;
 	}
 
+
+	/**
+	 * The measuring/printing loop, which runs on an independent thread.
+	 */
 	@Override
 	public void run() {
-		int lastProcCount = 0;
+
+		// The counter of processed cycles of the measuring/printing loop.
 		int loopCount = 0;
+
+		// Variables for storing counter/timer values measured at the last cycle.
+		int lastProcCount = 0;
 		long lastNanoTime = System.nanoTime();
+
+		// The measuring/printing loop.
 		while (this.continuable) {
 
-			// ループウェイト
+			// Wait a short time (PROC_CNT_INTERVAL_WAIT) for each cycle.
 			try {
 				Thread.sleep(PROC_CNT_INTERVAL_WAIT);
 			} catch (InterruptedException ie) {
@@ -88,16 +145,21 @@ public class PerformanceValuePrinter implements Runnable {
 			}
 
 
-			// 以下、エンジンから最新の計測値を取得
-			int currentProcCount = 0;// VMでの現在の累積命令実行数（int なので頻繁に一周する事に注意）
-			String[] currentOpcodeNames = new String[0]; // 現在の実行対象命令のオペコード（複数あり得るので配列）の文字列表現
+			// The counter of the currently executed instructions (should be reset before overflowing).
+			int currentProcCount = 0;
+
+			// The names of the currently executed instructions.
+			String[] currentOpcodeNames = new String[0];
+
+			// Measure performance values from the engine or the VM.
 			try {
 
-				// 通常のスクリプト処理の場合
+				// When we should measure/print performances of the VnanoEngine.
+				// (When we are executing a Vnano script.)
 				if (this.vnanoEngine != null) {
 					Map<String, Object> performanceMap = this.vnanoEngine.getPerformanceMap();
 
-					// 実行開始後、計測可能なタイミングにまだ達していない時には、値が格納されていない(仕様)ので検査してから参照
+					// Note: When no performance value has not been measured yet, no value is stored to the performance map.
 					if (performanceMap.containsKey(PerformanceKey.EXECUTED_INSTRUCTION_COUNT_INT_VALUE)) {
 						currentProcCount = (int)performanceMap.get(PerformanceKey.EXECUTED_INSTRUCTION_COUNT_INT_VALUE);
 					}
@@ -106,15 +168,13 @@ public class PerformanceValuePrinter implements Runnable {
 					}
 				}
 
-				// コンパイルしてファイルにダンプしたVRILコードを、コマンドラインモードで読み込んで処理している場合
+				// When we should measure/print performances of the VirtualMachine.
+				// (When we are executing a VRIL assembly code.)
 				if (this.virtualMachine != null) {
 
-					// virtualMachine は本来は表層ではなく内側のクラスなので、上でのパフォーマンスマップのように
-					//「無い値は詰めない」という対処はされておらず、常に何らかの値を返す。
-					// 従って中身で判断する必要がある。型も汎用的な型ではなくエンジン内部の実装用クラスのままだったりするので変換する。
-
-					currentProcCount = this.virtualMachine.getExecutedInstructionCountIntValue(); // まだ何も実行していない場合は 0
-					OperationCode[] currentOpcodes = this.virtualMachine.getCurrentlyExecutedOperationCodes(); // まだ値が無い場合は空配列
+					// Note: The following methods of the VM always return values, even when no performance value has not been measured yet.
+					currentProcCount = this.virtualMachine.getExecutedInstructionCountIntValue(); // Returns 0 when no value has not been measured.
+					OperationCode[] currentOpcodes = this.virtualMachine.getCurrentlyExecutedOperationCodes(); // Returns an empty array when no value has not been measured.
 					currentOpcodeNames = new String[ currentOpcodes.length ];
 					for (int i=0; i<currentOpcodes.length; i++) {
 						currentOpcodeNames[i] = currentOpcodes[i].toString();
@@ -126,10 +186,14 @@ public class PerformanceValuePrinter implements Runnable {
 			}
 
 
-			// オペコードごとの検出回数を控える（重み付き）カウンタマップに、現在実行中の命令のオペコードの分（下記参照）を加算
+			// Add the value of the counter of the instruction execution frequency (stored in the operationCodeCountMap).
 			if (currentOpcodeNames.length != 0) {
-				// カウンタの増分は通常は 1 で、N個の命令の同時に実行されている場合にはN等分する
+
+				// When only 1 operation code isdetected, we add 1 to its counter.
+				// When multiple operation codes are detected at the same moment, 
+				// we add 1/N to their counters, where N is the number of operation codes detected at the same moment.
 				double opcodeCountWeight = 1.0 / currentOpcodeNames.length;
+
 				for (String opcode: currentOpcodeNames) {
 					double latestValue = this.operationCodeCountMap.containsKey(opcode) ?
 							this.operationCodeCountMap.get(opcode).doubleValue() : 0.0;
@@ -140,20 +204,21 @@ public class PerformanceValuePrinter implements Runnable {
 			this.operationCodeCountTotal += currentOpcodeNames.length;
 
 
-			// 前回取得した累積命令数カウンタ値の差分と、取得間隔の時間から、1秒あたりのVM命令処理数（IPS）を求める
+			// Calculate the value of Instructions-Per-Second (IPS).
 			long currentNanoTime = System.nanoTime();
-			double countIntervalSec = (currentNanoTime - lastNanoTime) * 1.0E-9;    // 秒単位の取得間隔時間(1ナノ秒は10のマイナス9乗秒)
-			double procIps = (currentProcCount - lastProcCount) / countIntervalSec; // 1秒あたり命令処理数
+			double countIntervalSec = (currentNanoTime - lastNanoTime) * 1.0E-9;
+			double procIps = (currentProcCount - lastProcCount) / countIntervalSec;
 
-			// カウンタが int の都合上、取得は頻度に行うが、すぐ表示するには高頻度すぎるので、平均しつつたまに表示する
+			// To prevent overflow, we get and reset the value of the counter frequently, and IPS value to the following variable.
+			// Then, at each timing to print results, we calculate the average of IPS from the following sum value, and print it.
 			this.procIpsSum += procIps;
 
-			// 通常は上での取得値を「前回の値」として控えて、そのままループの次サイクルへ
+			// Store values measured at this cycle.
 			lastProcCount = currentProcCount;
 			lastNanoTime = currentNanoTime;
 			loopCount++;
 
-			// ただし、だいたい1秒ごとくらいのタイミング（ラグあり）で性能値を表示し、平均用変数やループカウンタなどもリセットする
+			// When the processed cycles has reached to the printing interval cycles, print results and reset counters.
 			if (loopCount == PROC_CNT_GETS_PER_SEC) {
 				System.out.println("================================================================================");
 				String timestamp = new Timestamp(System.currentTimeMillis()).toString();
@@ -188,7 +253,7 @@ public class PerformanceValuePrinter implements Runnable {
 		}
 	}
 
-	// VM動作速度の double 値を、「 1.23 GHz 」とか表示用のいい感じの形に調整する
+	// Format the value of the VM drive speed to be printed.
 	private String formatIpsValue(double ipsRawValue) {
 		double value = ipsRawValue;
 		String unitPrefix = "";
@@ -207,7 +272,7 @@ public class PerformanceValuePrinter implements Runnable {
 		return formattedValue;
 	}
 
-	// RAM使用量とかで使うバイト数の long 値を、「 1.23 GiB 」とか表示用のいい感じの形に調整する
+	// Format the value of the RAM usage to be printed..
 	private String formatRamBytes(long ramBytes) {
 		double value = ramBytes;
 		String unitPrefix = "";
@@ -226,21 +291,25 @@ public class PerformanceValuePrinter implements Runnable {
 		return formattedValue;
 	}
 
-	// 命令のオペコードの検出回数マップを、頻度分布として表示用のいい感じの形に調整する
+	/**
+	 * Format the instruction execution frequencies to be printed.
+	 * 
+	 * @param operationCodeCountMap The map in which detected counters of operation codes are stored.
+	 */
 	private String formatInstructionFrequency(HashMap<String, Double> operationCodeCountMap) {
 		StringBuilder builder = new StringBuilder();
 		String eol = System.getProperty("line.separator");
 
-		// 値の加工用にマップを clone し、その キーと値のセット（加工用）を取得
+		// Clone the map in which detected counters of  operation codes are stored.
 		@SuppressWarnings("unchecked")
 		HashMap<String, Double> modifiedMap = (HashMap<String, Double>)operationCodeCountMap.clone();
 		Set<Map.Entry<String, Double>> modifiedEntrySet = modifiedMap.entrySet();
 
-		// 各要素（オペコードと検出回数をまとめたEntry）を検出回数基準で降順ソート
+		// Sort entries (key-value pairs) in the map in the descending order of counters.
 		List<Map.Entry<String, Double>> modifiedEntryList = new ArrayList<>(modifiedEntrySet);
 		Collections.sort(modifiedEntryList, new CountEntryComparator());
 
-		// 検出回数を総和で割って 0 ～ 1 の頻度値に規格化する（パーセンテージ変換は後で丸めと一緒に行う）
+		// Convert conters to frequencies (from 0 to 1).
 		double total = 0.0;
 		for (Map.Entry<String, Double> modifiedEntry: modifiedEntryList) {
 			total += modifiedEntry.getValue().doubleValue();
@@ -249,34 +318,43 @@ public class PerformanceValuePrinter implements Runnable {
 			modifiedEntry.setValue( Double.valueOf(modifiedEntry.getValue().doubleValue() / total) );
 		}
 
-		// 頻度値をパーセンテージに変換しながら、丸めて文字列化し、バッファに書き込んでいく
+		// Formatters to format instruction execution frequencies / counters.
 		DecimalFormat percentageFormatter = new DecimalFormat("0.00");
-		DecimalFormat countFormatter = new DecimalFormat("0.#"); // 複数同時検出の際に 1/N する重み付きカウントなので、小数部が付く場合もある
+		DecimalFormat countFormatter = new DecimalFormat("0.#"); // Note: This value may have fraction part.
+
+		// Convert and print frequencies.
 		for (Map.Entry<String, Double> modifiedEntry: modifiedEntryList) {
 
-			// 表示用のオペレーションコード名を、空白で同じ長さに揃える (REFELEMが最長で7文字なので、7文字に揃える)
+			// Align lengths of operation codes to be printed, by appending spaces to end of them.
+			// The length of the longest operation code is 7 chars (REFELEM and so on), so align to the 7 chars. 
 			String opcode = String.format("%7s", modifiedEntry.getKey().toString());
 
-			// 加工済み頻度値（パーセンテージ）
-			String percentage = percentageFormatter.format( modifiedEntry.getValue().doubleValue() * 100.0 ); // double の桁数を丸める
-			percentage = String.format("%6s", percentage); // 文字列の長さを揃える
+			// Convert to each frequency (from 0 to 1) to a percentage, and round it.
+			String percentage = percentageFormatter.format( modifiedEntry.getValue().doubleValue() * 100.0 ); // Round the double value.
+			percentage = String.format("%6s", percentage); // Align the length of chars.
 
-			// 未加工の頻度値（カウント数）
+			// Raw counter value.
 			String count = countFormatter.format( operationCodeCountMap.get(modifiedEntry.getKey()) );
 
-			// 表示（バッファ書き込み）
+			// Print the above values.
 			builder.append("    - " + opcode + " : " + percentage + " %   (" + count + " count)" + eol);
 		}
 		return builder.toString();
 	}
 
+	/**
+	 * The comparator for sorting entries in the map storing counters of operation codes.
+	 */
 	class CountEntryComparator implements Comparator<Map.Entry<String, Double>> {
 		@Override
 		public int compare(Map.Entry<String, Double> a, Map.Entry<String, Double> b) {
-			//double diff = a.getValue() - b.getValue(); // 昇順
-			double diff = b.getValue() - a.getValue(); // 降順(最頻命令を上にしたいのでこっちの方がいい)
+			//double diff = a.getValue() - b.getValue(); // Ascending order.
+			double diff = b.getValue() - a.getValue(); // Descending order.
 			return (int)Math.signum(diff);
-			// ※ signum の戻り値は double だが絶対値が 1.0 か 0.0 のみで2進で完全表現できるので、int に精度落ちなくキャストできる
+			
+			// Note: The return value of the signum(...) is double, 
+			//   but it takes only values of 0 or 1, and it can be completely expressed in binary.
+			//   So we can cast it to int without any numerical errors.
 		}
 	}
 
