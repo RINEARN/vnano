ffa9c42fed580faa2f148460b3e473c6724cf64c
RINEARN <commit@rinearn.com>
2022-08-08 11:19:02 +0900

    Refactor: spec: clean-up/translate comments in ConfirmationType

--- src/org/vcssl/nano/spec/ConfirmationType.java
+++ src/org/vcssl/nano/spec/ConfirmationType.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright(C) 2020-2022 RINEARN
+ * This software is released under the MIT License.
+ */
+
 package org.vcssl.nano.spec;
 
+/**
+ * The enum to distinguish types of confirmation messages.
+ */
 public enum ConfirmationType {
+
+	/** Represents the message confirming whether the user allow the requested permission. */
 	PERMISSION_REQUESTED,
+
+	/**
+	 * Represents the message confirming whether the user want to allow automatically the same kind of permission,
+	 * which had already been allowed in the currently running script.
+	 */
 	ALLOW_SAME_PERMISSION_AUTOMATICALLY,
 }
