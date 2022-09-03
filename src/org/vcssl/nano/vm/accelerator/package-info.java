b4678af5dac653f3609bf5dc284983058be16e83
RINEARN <commit@rinearn.com>
2022-09-04 00:23:31 +0900

    Refactor: vm: accelerator: clean-up/translate comments in package-info

--- src/org/vcssl/nano/vm/accelerator/package-info.java
+++ src/org/vcssl/nano/vm/accelerator/package-info.java
@@ -1,8 +1,17 @@
 /**
- * <p>
- * 実装の単純さよりも処理速度を優先した、
- * 仮想プロセッサ（{@link org.vcssl.nano.vm.processor processor}パッケージ）
- * の一部機能の高速実装（使用は任意）を提供するパッケージです。
- * </p>
+ * This package provides a high-speed implementation of {@link org.vcssl.nano.vm.processor} package.
+ * 
+ * The outer-frame of the compiler provided by this package is {@link Accelerator} class,
+ * and other classes are internal components.
+ * 
+ * This package contains far many classes than the "processor" package, and code in each classes is also more complicated.
+ * Most of comments are the temporary note of/for the author of the class (written in Japanese).
+ * Probably we will try to supplement/translate comments in this package in future, but it is undecided when it will be done.
+ * 
+ * Hence, we hardly recommend modifying code of classes in this package for now.
+ * You can avoid to use this package in the script engine, by setting "ACCELERATOR_ENABLED" option to false.
+ * If you do the above, all functions performed by this package will be switched to use only "processor" package.
+ * The code of the classes in the "processor" package are far easy to read/modify than this package 
+ * (though its processing speed is far slower than this package).
  */
 package org.vcssl.nano.vm.accelerator;
