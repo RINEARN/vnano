e557604d1084f38a13f35b3acac117d8280863e3
RINEARN <commit@rinearn.com>
2022-09-29 21:37:46 +0900

    Refactor: clean-up comments in VnanoException

--- src/org/vcssl/nano/VnanoException.java
+++ src/org/vcssl/nano/VnanoException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2017-2020 RINEARN (Fumihiro Matsui)
+ * Copyright(C) 2017-2022 RINEARN
  * This software is released under the MIT License.
  */
 
@@ -10,30 +10,9 @@ import java.util.Locale;
 import org.vcssl.nano.spec.ErrorMessage;
 import org.vcssl.nano.spec.ErrorType;
 
-//Documentation:  https://www.vcssl.org/en-us/dev/code/main-jimpl/api/org/vcssl/nano/VnanoException.html
-//ドキュメント:   https://www.vcssl.org/ja-jp/dev/code/main-jimpl/api/org/vcssl/nano/VnanoException.html
 
 /**
- * <span class="lang-en">
  * The exception class thrown when the (usual type of) error detected for contents or processing of scripts
- * </span>
- * <span class="lang-ja">
- * スクリプトコードの内容や実行過程に, 通常想定される範囲内のエラーが検出された際にスローされる例外です
- * </span>
- * .
- *
- * <p>
- * &raquo; <a href="../../../../src/org/vcssl/nano/VnanoException.java">Source code</a>
- * </p>
- *
- * <hr>
- *
- * <p>
- * | <a href="../../../../api/org/vcssl/nano/VnanoException.html">Public Only</a>
- * | <a href="../../../../api-all/org/vcssl/nano/VnanoException.html">All</a> |
- * </p>
- *
- * @author RINEARN (Fumihiro Matsui)
  */
 @SuppressWarnings("serial")
 public class VnanoException extends Exception implements Cloneable {
@@ -59,7 +38,8 @@ public class VnanoException extends Exception implements Cloneable {
 		return clonedVnanoException;
 	}
 
-	// clone() 内で使用する用で、そこで各フィールドの値をコピーして設定する必要がある
+	// Used only in clone() method.
+	// Don't change this to "public" method, because error messages of VnanoException(s) should not be hardcoded.
 	private VnanoException(String message, Throwable errorCauseThrowable) {
 		super(message, errorCauseThrowable);
 	}
@@ -71,6 +51,10 @@ public class VnanoException extends Exception implements Cloneable {
 
 	public VnanoException(ErrorType errorType) {
 		this(errorType, (String)null, -1);
+	}
+
+	public VnanoException(ErrorType errorType, Throwable cause) {
+		this(errorType, (String)null, cause);
 	}
 
 	public VnanoException(ErrorType errorType, String fileName, int lineNumber) {
@@ -89,37 +73,35 @@ public class VnanoException extends Exception implements Cloneable {
 		this(errorType, errorWords, (String)null, -1);
 	}
 
-	public VnanoException(ErrorType errorType, String[] errorWords, Throwable cause, String fileName, int lineNumber) {
-		super(ErrorMessage.generateErrorMessage(errorType, errorWords, Locale.getDefault()), cause); // このLocaleは本来どうにかして設定から引っ張ってきたいけど…後々で要検討
-		this.errorType = errorType;
-		this.errorWords = errorWords;
-		this.fileName = fileName;
-		this.lineNumber = lineNumber;
-	}
-
 	public VnanoException(ErrorType errorType, String[] errorWords, String fileName, int lineNumber) {
-		super(ErrorMessage.generateErrorMessage(errorType, errorWords, Locale.getDefault())); // このLocaleは本来どうにかして設定から引っ張ってきたいけど…後々で要検討
+		super(ErrorMessage.generateErrorMessage(errorType, errorWords, Locale.getDefault())); // We want to get this Locale from settings but...
 		this.errorType = errorType;
 		this.errorWords = errorWords;
 		this.fileName = fileName;
 		this.lineNumber = lineNumber;
-	}
-
-	public VnanoException(ErrorType errorType, String[] errorWords, Throwable cause) {
-		this(errorType, errorWords, cause, (String)null, -1);
 	}
 
 	public VnanoException(ErrorType errorType, String errorWord, Throwable cause) {
 		this(errorType, new String[] {errorWord}, cause);
 	}
 
-	public VnanoException(ErrorType errorType, Throwable cause) {
-		this(errorType, (String)null, cause);
+	public VnanoException(ErrorType errorType, String[] errorWords, Throwable cause) {
+		this(errorType, errorWords, cause, (String)null, -1);
 	}
+
 
 	public VnanoException(ErrorType errorType, Throwable cause, String fileName, int lineNumber) {
 		this(errorType, (String[])null, cause);
 	}
+
+	public VnanoException(ErrorType errorType, String[] errorWords, Throwable cause, String fileName, int lineNumber) {
+		super(ErrorMessage.generateErrorMessage(errorType, errorWords, Locale.getDefault()), cause); // We want to get this Locale from settings but...
+		this.errorType = errorType;
+		this.errorWords = errorWords;
+		this.fileName = fileName;
+		this.lineNumber = lineNumber;
+	}
+
 
 	public ErrorType getErrorType() {
 		return this.errorType;
@@ -149,16 +131,16 @@ public class VnanoException extends Exception implements Cloneable {
 		this.fileName = fileName;
 	}
 
+	public boolean hasLineNumber() {
+		return this.lineNumber != LINE_NUMBER_DEFAULT_VALUE;
+	}
+
 	public int getLineNumber() {
 		return this.lineNumber;
 	}
 
 	public void setLineNumber(int lineNumber) {
 		this.lineNumber = lineNumber;
-	}
-
-	public boolean hasLineNumber() {
-		return this.lineNumber != LINE_NUMBER_DEFAULT_VALUE;
 	}
 
 	public void setLocale(Locale locale) {
