c8ed17a1b82c2844f3a84b19d018cd3cf07730e7
RINEARN <commit@rinearn.com>
2022-10-18 19:15:26 +0900

    Update: version: 0.9.4 (RC2 / Open Beta4)

--- src/org/vcssl/nano/spec/EngineInformation.java
+++ src/org/vcssl/nano/spec/EngineInformation.java
@@ -17,7 +17,7 @@ public class EngineInformation {
 	public static final String ENGINE_NAME = "RINEARN Vnano Engine";
 
 	/** The version of this script engine. */
-	public static final String ENGINE_VERSION = "0.9.3";
+	public static final String ENGINE_VERSION = "0.9.4";
 
 	/** The extension of script files which can run on this script engine. */
 	public static final String[] EXTENTIONS = { "vnano" };
